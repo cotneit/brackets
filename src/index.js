8d5cd5dbfddd5d2db712a221e0e17458bb4935e7
Vladyslav Karasov <36513243+cotneit@users.noreply.github.com>
2020-10-11 17:42:40 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const queque = [];
+  const brackets = {};
+
+  // create openingBracket: closingBracket pairs
+  bracketsConfig.forEach((element) => {
+    brackets[element[0]] = element[1];
+  })
+
+  for (let i = 0; i < str.length; i++) {
+    const current = str[i];
+
+    if (brackets[current] && brackets[current] !== queque[queque.length - 1]) {
+      queque.push(current);
+    } else if (brackets[queque.pop()] !== current) {
+      return false;
+    }
+  }
+
+  if (queque.length === 0) {
+    return true;
+  }
+
+  return false;
 }
